a0011b7872483b0d6351eca96dc56793f1d9ecfd
Jogi Dhimas Anditya <50067691+JogiAnditya@users.noreply.github.com>
2023-04-28 10:42:41 +0700

    v1.0 - menambahkan fungsi php
    Menambahkan fungsi php pada calculator.php

--- calculator.php
+++ calculator.php
@@ -1,3 +1,42 @@
+<?php
+//menambil data dari form
+    $num1 = $_POST['num1'] ?? 0;
+    $num2 = $_POST['num2'] ?? 0;
+    $op = $_POST['op'] ?? '+';
+
+    // validasi nilai input
+    if (!is_numeric($num1) || !is_numeric($num2)) {
+        $error = "Please enter valid numbers";
+    } elseif ($op == '/' && $num2 == 0) {
+        $error = "Cannot divide by zero";
+    } else {
+        $error = "";
+    }
+
+    // operasional aritmatika
+    switch ($op) {
+        case '+':
+            $result = $num1 + $num2;
+            break;
+        case '-':
+            $result = $num1 - $num2;
+            break;
+        case '*':
+            $result = $num1 * $num2;
+            break;
+        case '/':
+            $result = $num1 / $num2;
+            break;
+        default:
+            $result = "Invalid operation";
+    }
+    // Menampilkan hasil
+    $result = number_format($result, 2);
+
+    // Membuat array operasi aritmatika
+    $operations = ['+', '-', '*', '/'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,13 +45,12 @@
 </head>
 <body>
     <h1>PHP Calculator</h1>
-    <p>This is a single file PHP project that shows off some of my PHP skills.</p>
-    <p>It is a simple calculator that can perform basic arithmetic operations on two numbers.</p>
+    <p>Kalkulator sederhana yang dapat melakukan operasi aritmatika dasar pada dua angka</p>
     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        <label for="num1">Enter first number:</label>
+        <label for="num1">Masukkan angka pertama:</label>
         <input type="text" id="num1" name="num1" value="<?php echo htmlspecialchars($num1); ?>">
         <br>
-        <label for="op">Select an operation:</label>
+        <label for="op">Pilih Operasi:</label>
         <select id="op" name="op">
             <?php foreach ($operations as $operation): ?>
                 <option value="<?php echo $operation; ?>" <?php if ($op == $operation) echo 'selected'; ?>>
@@ -21,7 +59,7 @@
             <?php endforeach; ?>
         </select>
         <br>
-        <label for="num2">Enter second number:</label>
+        <label for="num2">Masukkan angka kedua:</label>
         <input type="text" id="num2" name="num2" value="<?php echo htmlspecialchars($num2); ?>">
         <br>
         <input type="submit" value="Calculate">
